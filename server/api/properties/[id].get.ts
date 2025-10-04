f1085dad97aa72fdc1803b141ececd1740dee5a4
John <john.mbiddulph@gmail.com>
2025-10-04 16:16:53 +0100

    verify location

--- server/api/properties/[id].get.ts
+++ server/api/properties/[id].get.ts
@@ -37,6 +37,8 @@ export default defineEventHandler(async (event) => {
         qr_code,
         nfc_id,
         emergency_access_enabled,
+        latitude,
+        longitude,
         created_at,
         updated_at
       `)
